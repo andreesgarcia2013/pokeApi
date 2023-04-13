e7551de905d22fd0a3d3cbd195eb4afb1725f576
andreesgarcia2013 <18030514@itcelaya.edu.mx>
2023-04-12 22:05:32 -0600

    Paginacion y manejo de errores

--- src/views/PokeView.vue
+++ src/views/PokeView.vue
@@ -1,34 +1,36 @@
 import axios from 'axios';
 <script setup>
-    import axios from 'axios';
+    // import axios from 'axios';
     import { useRoute, useRouter } from "vue-router";
+    import {useGetData} from '@/composables/getData'
 import { ref } from 'vue';
     const route= useRoute()
     const router=useRouter()
 
-    const poke=ref({})
+    const {data, getData, loading, errorData}=useGetData()
+
+    // const poke=ref({})
     const back=()=>{
         router.push('/pokemons')
     }
-    const getData=async()=>{
-        try {
-            const {data}=await axios.get(`https://pokeapi.co/api/v2/pokemon/${route.params.name}`)
-            poke.value=data
-            console.log(data);            
-        } catch (error) {
-            console.log(error);
-            poke.value=null
-        }
-    }
-getData();
+    // const getData=async()=>{
+    //     try {
+    //         const {data}=await axios.get(`https://pokeapi.co/api/v2/pokemon/${route.params.name}`)
+    //         poke.value=data
+    //         console.log(data);            
+    //     } catch (error) {
+    //         console.log(error);
+    //         poke.value=null
+    //     }
+    // }
+getData(`https://pokeapi.co/api/v2/pokemon/${route.params.name}`);
 </script>
 <template>
-    <div v-if="poke">
-        <img :src="poke.sprites?.front_default" alt="">
+    <div v-if="loading">Cargando...</div>
+    <div v-if="errorData" class="alert alert-danger">No existe el pokemon</div>
+    <div v-if="data">
+        <img :src="data.sprites?.front_default" alt="">
         <h1>pokemon name: {{ $route.params.name }}</h1>
     </div>
-    <h1 v-else>
-        no existe el pokemon
-    </h1>
     <button class="btn btn-secondary" @click="back">back</button>
 </template>
--- src/views/PokemonsView.vue
+++ src/views/PokemonsView.vue
@@ -1,26 +1,31 @@
 <script setup>
-    import axios from 'axios';
-    import {ref} from 'vue';
-    import {RouterLink} from 'vue-router'
+import { ref } from 'vue';
+import { RouterLink } from 'vue-router'
+import { useGetData } from '@/composables/getData'
+// const pokemons=ref([])
 
-    const pokemons=ref([])
-
-    const getData=async()=>{
-        try {
-            const {data}= await axios.get('https://pokeapi.co/api/v2/pokemon')
-            pokemons.value=data.results
-        } catch (error) {
-            console.log(error);
-        }
-    }
-    getData();
+const { data, getData, loading, errorData } = useGetData()
+// const getData=async()=>{
+//     try {
+//         const {data}= await axios.get('https://pokeapi.co/api/v2/pokemon')
+//         pokemons.value=data.results
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
+getData("https://pokeapi.co/api/v2/pokemon");
 </script>
 <template>
     <h1>Pokemons</h1>
-    <div v-for="pokemon in pokemons">
-        
-        <h1>
-            <router-link :to="`/pokemons/${pokemon.name}`">{{ pokemon.name }}</router-link>
-        </h1>
+    <div v-if="loading">Cargando...</div>
+    <div v-if="errorData" class="alert alert-danger">{{ errorData }}</div>
+    <div v-if="data">
+        <button :disabled="!data.previous" class="btn btn-outline-primary" @click="getData(data.previous)"> prev</button>
+        <button :disabled="!data.next" class="btn btn-outline-primary me-3" @click="getData(data.next)"> next</button>
+        <div v-for="pokemon in data.results">
+            <h1>
+                <router-link :to="`/pokemons/${pokemon.name}`">{{ pokemon.name }}</router-link>
+            </h1>
+        </div>        
     </div>
 </template>
